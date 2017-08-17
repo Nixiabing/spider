b29d2830bb036598e3ae2f7b734f39b036cbc848
Bob <240366771@qq.com>
2017-08-17 21:05:46 +0800

    20170817

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+:s
+echo 程序已开始执行，5分钟扫描一次
+ping localhost -n 300 > nul
+scrapy crawl weibo
+scrapy crawl web
+  
+goto s
+pause
---
+++ tutorial/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class WeiboItem(scrapy.Item):
+    weiboId = scrapy.Field()#微博ID（唯一）
+    weiboUser = scrapy.Field()#微博用户名
+    weiboHome = scrapy.Field()#微博地址
+    weiboContent = scrapy.Field()#微博内容
+    weiboType = scrapy.Field()#检测重要性（0：轻微；2：严重）
+
+class WebItem(scrapy.Item):
+    webId = scrapy.Field()#报道ID（唯一）
+    webSource = scrapy.Field()#报道媒体
+    webTitle = scrapy.Field()#报道标题
+    webDes = scrapy.Field()#主要内容
+    webUrl = scrapy.Field()#报道网址
+    webType = scrapy.Field()#检测重要性（0：轻微；2：严重）
